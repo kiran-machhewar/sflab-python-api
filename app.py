e8115a137f945d11ef68a006058d41b9c42c1df7
Kiran Machhewar <smachhewar@gmail.com>
2018-12-28 17:33:02 -0800

    new changes to support post

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request, Response
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! - Kiran Machhewar'
+    return 'Hello World! - Kiran Machhewar' 
+    
+@app.route('/json-post', methods=['POST'])
+def json_post():
+    req_data = request.get_json()
+    name = req_data['name']
+    email = req_data['email']
+    resp = Response('''
+           { "Passed Name": "Kiran",
+           "Passed Email": "smachhewar@gmail.com" }''')
+    resp.headers['Content-Type'] = 'application/json'
+    return resp;
 
 if __name__ == '__main__':
     app.run()
+    
