72c85f07fefdb2b35d021c5cdc4a942c865b5e98
Kiran Machhewar <kiranmachhewar@Kirans-MacBook-Pro.local>
2019-08-25 16:09:12 -0700

    removing non required code

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World! - Kiran Machhewar' 
     
-@app.route('/json-post', methods=['POST'])
-def json_post():
-    req_data = request.get_json()
-    name = req_data['name']
-    email = req_data['email']
-    resp = Response('''
-           { "Passed Name": "Kiran",
-           "Passed Email": "smachhewar@gmail.com" }''')
-    resp.headers['Content-Type'] = 'application/json'
-    return resp
-
 @app.route('/oauth/callback',methods=['GET'])
 def handle_oauth_callback():    
     code = request.args.get('code')   
