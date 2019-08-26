9fee196dd4c5d46603a4e00357737ee4917ae25d
Kiran Machhewar <kiranmachhewar@Kirans-MacBook-Pro.local>
2019-08-25 19:50:26 -0700

    support for service html files

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, Response
+from flask import render_template
 import os.path
 import util.kmlib
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! - Kiran Machhewar' 
+    return render_template('index.html', message='SF Python API by Kiran Machhewar') 
     
 @app.route('/oauth/callback',methods=['GET'])
 def handle_oauth_callback():    
     code = request.args.get('code')   
-    access_token = util.kmlib.getAccessTokenByCode(code,request.args.get('state'),os.environ['CLIENT_ID'],os.environ['SECRETE_KEY'])
-    resp = Response('{ "Passed Name": "Code is '+code+'", "Access Token":"'+access_token+'"}')
-    return resp
+    access_token = util.kmlib.getAccessTokenByCode(code,request.args.get('state'),os.environ['CLIENT_ID'],os.environ['SECRETE_KEY'])    
+    return render_template('ouath_result.html', access_token=access_token)    
 
 if __name__ == '__main__':    
     app.run()
